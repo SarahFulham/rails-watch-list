f69098dff70b8574e1de6fdd083b327e7dfd8721
sarah fulham <sarahlfulham@gmail.com>
2022-08-12 12:11:50 -0400

    added stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/index'
+  root to: 'lists#index'
+  resources :lists, only: %i[index show create new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
